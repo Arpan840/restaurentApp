7ca8f761bcf690d55d22a0b30232dd0b52ef45b3
Arpan Das <arpan.das@klythe.com>
2023-08-26 23:38:51 +0530

    get order function added

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
-import naveMenue from './JonFiles/naveMenu.json' assert { type: "json" }
-import moreAboutUs from './JonFiles/moreAboutUs.json' assert {type:"json"}
-function sideOptions(){
-    let navMeueItems = '';
-    naveMenue.map((i)=>{
-     navMeueItems +=`
+import naveMenue from "./JonFiles/naveMenu.json" assert { type: "json" };
+import moreAboutUs from "./JonFiles/moreAboutUs.json" assert { type: "json" };
+function sideOptions() {
+  let navMeueItems = "";
+  naveMenue.map((i) => {
+    navMeueItems += `
      <div class='navmin'>
      <p>${i.id}<p>
      <p> ${i.name}</p>
      </div>
-     `
-    })
-    document.querySelector('.navMenu').innerHTML = navMeueItems
+     `;
+  });
+  document.querySelector(".navMenu").innerHTML = navMeueItems;
 }
-addEventListener('DOMContentLoaded', sideOptions)
+addEventListener("DOMContentLoaded", sideOptions);
 
-function aboutUsOptions(){
-    let moreAbtUsItems = '';
-    moreAboutUs.map((i)=>{
-        moreAbtUsItems +=`
+function aboutUsOptions() {
+  let moreAbtUsItems = "";
+  moreAboutUs.map((i) => {
+    moreAbtUsItems += `
      <div class='navmin'>
      <p>${i.id}<p>
      <p> ${i.name}</p>
      </div>
-     `
-    })
-    document.querySelector('.AboutUsMenu').innerHTML = moreAbtUsItems
+     `;
+  });
+  document.querySelector(".AboutUsMenu").innerHTML = moreAbtUsItems;
 }
-addEventListener('DOMContentLoaded', aboutUsOptions)
-let foodDetails
-async function getMenu(){
-    try {
-        let foodItems = await fetch(' https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json')
-      
-        foodDetails =await foodItems.json()
-      
-   } catch (error) {
-    console.log(error)
-   }
-   let foodMenu = ''
-   foodDetails.map((i)=>{
-    foodMenu+=`
+addEventListener("DOMContentLoaded", aboutUsOptions);
+let foodDetails;
+async function getMenu() {
+  try {
+    let foodItems = await fetch(
+      " https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json"
+    );
+
+    foodDetails = await foodItems.json();
+  } catch (error) {
+    console.log(error);
+  }
+  let foodMenu = "";
+  foodDetails.map((i) => {
+    foodMenu += `
     <div class="foodItem">
     <div class="foodItemPic">
       <img
@@ -79,8 +80,37 @@ async function getMenu(){
       </div>
     </div>
   </div>
-    `
-   })
- document.querySelector('.foodItems').innerHTML = foodMenu
+    `;
+  });
+  document.querySelector(".foodItems").innerHTML = foodMenu;
+  async function takeOrder() {
+    return new Promise(async (resolve) => {
+      await new Promise((resolveTimeout) => setTimeout(resolveTimeout, 2500));
+
+      const order = {
+        items: [],
+      };
+
+      const burgerItems = foodDetails.filter((item) => item.name === "burgers");
+      const randomBurgers = getRandomItems(burgerItems, 3);
+
+      order.items = randomBurgers;
+
+      resolve(order);
+    });
+  }
+
+  function getRandomItems(array, count) {
+    const shuffled = array.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, count);
+  }
 }
-addEventListener('DOMContentLoaded', getMenu)
+addEventListener("DOMContentLoaded", getMenu);
+document.querySelector(".addItems").addEventListener("click", async () => {
+  try {
+    const order = await takeOrder();
+    console.log("Order:", order);
+  } catch (error) {
+    console.error("Error taking order:", error);
+  }
+});
